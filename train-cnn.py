7ca120df337c5ce6e6bb3ad7e640e1b5c1a03b9b
Klemen Voncina <klemenvon@gmail.com>
2019-05-04 13:04:56 +0200

    Can't stop fucking up. Should be fixed now.

--- train-cnn.py
+++ train-cnn.py
@@ -17,7 +17,8 @@ save_dir = os.path.join(os.getcwd(), 'model_save')
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 print("data shape: {}".format(x_train.shape))
 
-#y_train = keras.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
 # Making the model
 model = Sequential()
@@ -48,6 +49,12 @@ model.add(layers.Activation('softmax'))
 opt = keras.optimizers.rmsprop(lr=0.00001,decay=1e-6)
 
 model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
+
+# Normalize data and data types
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
 
 # Train the model
 model.fit(x_train,y_train,batch_size=batch_size,epochs=epochs,validation_data=(x_test,y_test),shuffle=True)
